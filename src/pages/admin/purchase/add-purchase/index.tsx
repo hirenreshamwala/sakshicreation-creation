15f631158394c3cc74ea00da72db842deb019034
chauhan-janak <cjanak39@gmail.com>
2025-12-13 17:54:06 +0530

    feat: Add new component and page for creating and editing Quality Packaging purchase records.

--- src/pages/admin/purchase/add-purchase/index.tsx
+++ src/pages/admin/purchase/add-purchase/index.tsx
@@ -3,14 +3,27 @@ import SakshiNewPurchase from '@/component/Purchase/SakshiPurchase/AddPurchase'
 import { useRouter } from 'next/router'
 import React from 'react'
 
-function index({ isEditMode = false, purchaseId }: any) {
+function Index({ isEditMode = false, purchaseId }: any) {
   const router = useRouter()
   const { type } = router.query
+
+  // ⛔ Jab tak query ready nahi, kuch render mat karo
+  if (!router.isReady) {
+    return null // ya loader
+  }
+
   return (
     <div>
-      {router.isReady && type === "1" ? <QpNewPurchase />:<SakshiNewPurchase isEditMode={isEditMode} purchaseId={purchaseId} /> }
+      {type === "1" ? (
+        <QpNewPurchase />
+      ) : (
+        <SakshiNewPurchase
+          isEditMode={isEditMode}
+          purchaseId={purchaseId}
+        />
+      )}
     </div>
   )
 }
 
-export default index
+export default Index
