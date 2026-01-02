b9711c9990d9d9b494afe622fdfc943055baf4a1
janakdigitalk-create <janakdigitalk@gmail.com>
2026-01-02 16:29:53 +0530

    feat: add CustomTable2 component with pagination and row selection, and create Account Master page.

--- src/pages/admin/account-master/index.tsx
+++ src/pages/admin/account-master/index.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect, memo } from "react";
 import { Box, TableCell, Typography, Avatar, IconButton } from "@mui/material";
 import { useRouter } from "next/router";
 import ThemeButton from "@/component/common_component/themebutton";
-import ThemeChip from "@/component/common_component/themechip";
 import AddNewPartyDialog from "@/component/AddNewPartyDialog";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
@@ -24,7 +23,7 @@ import { accountMasterService } from "@/services/accountMaster.service";
 import _ from "lodash";
 import { StaticCompanyOptions } from "@/constants";
 import { useAppDispatch, useAppSelector } from "@/store";
-import { getAllCompaniesThunk } from "@/store/slices/compnaySlice";
+import { getAllCompanyNamesThunk } from "@/store/slices/companyNameSlice";
 
 interface Company {
   _id: string;
@@ -183,7 +182,7 @@ const AccountMasterPage: React.FC = memo(() => {
 
   // Load companies
   useEffect(() => {
-    dispatch(getAllCompaniesThunk())
+    if(!companies.length)dispatch(getAllCompanyNamesThunk())
   }, []);
 
   // Load account masters
